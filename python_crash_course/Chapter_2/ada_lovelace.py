925170e53121a782df87708b63f5fa2f34568ea3
klare-yang <klare0603@gmail.com>
2026-01-12 23:11:25 +0800

    1.12 night

---
+++ python_crash_course/Chapter_2/ada_lovelace.py
@@ -0,0 +1,10 @@
+name = "Ada LovelAce"
+
+print(name.upper())
+print(name.lower())
+print(name.title())
+
+first_name = "ada"
+last_name = "lovelace"
+full_name = f"{first_name} {last_name}"
+print(full_name)
