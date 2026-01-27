d463cb0022c83fe88fcb1b6eb80d9f73695992c6
klare-yang <klare0603@gmail.com>
2026-01-27 21:58:43 +0800

    1.27 afternoon

--- python_crash_course/Chapter_5/7_admin_login.py
+++ python_crash_course/Chapter_5/7_admin_login.py
@@ -1,4 +1,21 @@
 name_list = ['admin', 'bob', 'jackson']
-for name in name_list:
-    if name == 'admin':
-        print(f"Hello {name},")
+
+def check(list):
+    if list:
+        for name in list:
+            if name == 'admin':
+                print(f"Hello {name},woould you like to see a status report?")
+            else:
+                print(f"Hello {name},thank you for logging again.")
+    else:
+        print("We need to find some user.")
+
+check(name_list)
+
+# for name in name_list:
+#     del name_list[0]
+
+# name_list.clear()
+# del name_list[0:]
+
+check(name_list)
