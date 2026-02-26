3944f835ba126327cfed4787143bddfe7bca45b7
klare-yang <klare0603@gmail.com>
2026-02-26 23:42:21 +0800

    2.26 night

---
+++ python_crash_course/Chapter_6/1_person.py
@@ -0,0 +1,10 @@
+classmate = {
+    'first_name': 'Mingxu',
+    'last_name': 'Cui',
+    'age': 19,
+    'city': 'Dalian',
+    }
+
+for key, value in classmate.items():
+       print(f"\nKey: {key}")
+       print(f"Value: {value}")
